9f76585867f497c5854232d641aed5faca4838a0
ban4e <kocomst@gmail.com>
2024-01-03 18:34:28 +0300

    css-modules name generation

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,14 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
     plugins: [react()],
-    resolve: { alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }] }
-});
+    resolve: { alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }] },
+    css: {
+        devSourcemap: true,
+        modules: {
+            localsConvention: 'camelCase',
+            generateScopedName: mode === 'development' ? '[name]__[local]' : '_[local]_[hash:base64:2]'
+        }
+    }
+}));
