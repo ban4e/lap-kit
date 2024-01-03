557d0163d5ad582e90c37a44e53418c3d3ac56f3
ban4e <kocomst@gmail.com>
2024-01-03 19:05:38 +0300

    Add vite svgr

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import * as path from 'path';
 
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import dynamicImport from 'vite-plugin-dynamic-import';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-    plugins: [react()],
+    plugins: [react(), svgr(), dynamicImport()],
     resolve: { alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }] },
     css: {
         devSourcemap: true,
